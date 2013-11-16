c63006cd6cf70f78661a8ece3305d7ae680bb551
Nestor Malet <nmaletm@gmail.com>
2013-11-16 19:30:23 +0100

    Login control

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,5 +32,16 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-	public $components = array('DebugKit.Toolbar');
+    public $components = array(
+    	'DebugKit.Toolbar',
+        'Session',
+        'Auth' => array(
+            'loginRedirect' => array('controller' => 'pages', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'home')
+        )
+    );
+
+    public function beforeFilter() {
+        $this->Auth->allow('index', 'view');
+    }
 }
--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -65,11 +65,6 @@ class UsersController extends AppController {
         return $this->redirect(array('action' => 'index'));
     }
 
-    public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow('add'); // Letting users register themselves
-    }
-
     public function login() {
         if ($this->request->is('post')) {
             if ($this->Auth->login()) {
