94c6f6528c32b1a12baaf18c4653e1ef9d6f4d5f
Nestor Malet <nmaletm@gmail.com>
2013-11-16 19:26:58 +0100

    Login control

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,4 +3,26 @@ class User extends AppModel{
 
 	var $name = 'User';
 	var $primaryKey = 'id';
+
+	public $validate = array(
+		'username' => array(
+			'required' => array(
+				'rule' => array('notEmpty'),
+				'message' => "El nom d'usuari és necessari"
+			)
+		),
+		'password' => array(
+			'required' => array(
+				'rule' => array('notEmpty'),
+				'message' => 'La contrasenya és necessaria'
+			)
+		)
+	);
+
+	public function beforeSave($options = array()) {
+		if (isset($this->data[$this->alias]['password'])) {
+			$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+		}
+		return true;
+	}
 }
