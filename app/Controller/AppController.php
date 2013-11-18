b38a797ee8e078b752ffb3407172d3f1d045323f
Nestor Malet <nmaletm@gmail.com>
2013-11-18 08:45:38 +0100

    Push to server

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -51,7 +51,6 @@ class AppController extends Controller {
     );
 
     public $helpers = array(
-        'Session',
         'Html' => array('className' => 'BoostCake.BoostCakeHtml'),
         'Form' => array('className' => 'BoostCake.BoostCakeForm'),
         'Paginator' => array('className' => 'BoostCake.BoostCakePaginator'),
